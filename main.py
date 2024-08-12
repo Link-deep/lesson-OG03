c0fc09abe09c50b4f1ae0bf5daa53843ae4d2c67
Link-deep <link.deep@yandex.ru>
2024-08-12 11:44:35 +0500

    add code 1.2

--- main.py
+++ main.py
@@ -33,10 +33,12 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 
+black = (0, 0, 0)
+point = 0
 
 
-
-
+# Устанавливаем шрифт и размер
+font = pygame.font.Font(None, 74)
 
 
 
@@ -44,6 +46,10 @@ running = True
 
 while running:
     screen.fill(color) # заливаем цвет окна
+
+
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,9 +60,21 @@ while running:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
                 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                point += 1
 
     # отрисовка цели
     screen.blit(target_image, (target_x, target_y))
+
+    # Создаем текстовый объект
+    point_text = font.render(f'СЧЕТ {point}:0', True, black)
+
+    # Получаем прямоугольник текста и устанавливаем его в центр
+    text_rect = point_text.get_rect(center=(400, 300))
+    
+    # Отображаем текст на экране
+    screen.blit(point_text, text_rect)
+
+
 
     pygame.display.update()
 
