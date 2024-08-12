a3462be3fed2f1cb5478dc62ce24e5f2bbf36bcb
Link-deep <link.deep@yandex.ru>
2024-08-12 10:33:57 +0500

    mod 1.0

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # УСТАНАВЛИВАЕМ РАЗМЕР ЭКРАНА
+
+pygame.display.set_caption("ИГРА ТИР")  # ЗАГОЛОВОК ОКНА
+
+icon = pygame.image.load('icon.png')
+
+
+
+
+
+
 running = True
 
 while running:
