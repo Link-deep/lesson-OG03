0d136c9df4cbc0a02df0beafd51efc88f1084d26
Link-deep <link.deep@yandex.ru>
2024-08-12 10:54:18 +0500

    add const 1.0

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import pygame
+import random
 
 pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # УСТАНАВЛИВАЕМ РАЗМЕР ЭКРАНА
+# УСТАНАВЛИВАЕМ РАЗМЕР ЭКРАНА
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption("ИГРА ТИР")  # ЗАГОЛОВОК ОКНА
+# ЗАГОЛОВОК ОКНА
+pygame.display.set_caption("ИГРА ТИР")
 
-icon = pygame.image.load('icon.png')
+
+# добавляем икноку в окно
+icon = pygame.image.load('image/Icon_tir.png') # Иконка
+pygame.display.set_icon(icon)
+
+
+# Изображение цели
+target_image = pygame.image.load('image/purpose.png')
+
+# размер изображения
+target_width = 50
+target_height = 50
+
+# координаты цели
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
+
 
 
 
